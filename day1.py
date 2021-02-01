02b52abc8fe2492cf2ea12c068a5249c0c130b39
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-01 18:39:59 +0530

    commit

--- day1.py
+++ day1.py
@@ -5,4 +5,14 @@
 # print("print('what to print')")
 
 # Insert a newline 
-print("Hello" + " " + "Gaurav")
+# print("Hello" + " " + "Gaurav")
+
+#--->>> Day 1 - String Manipulation <<<---
+# String Concatenation is done with the "+" sign.
+# e.g. print("Hello " + "world")
+# New lines can be created with a backslash and n.
+
+print("Day 1" + "-" + "String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world"')
+print(("New lines can be created with a backslash and n."))
