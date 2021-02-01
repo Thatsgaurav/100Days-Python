8a948948cc9870cf4b82711c89196857abb2a56d
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-01 18:15:57 +0530

    commit

--- Hello_World.py
+++ Hello_World.py
@@ -1,3 +1,9 @@
-print('Day 1 - Python Print Function')
-print("The function is declared like this")
-print("print('what to print')")
+# Day 1 
+
+# print('Day 1 - Python Print Function')
+# print("The function is declared like this")
+# print("print('what to print')")
+
+# Insert a newline 
+print("Hello world!\nHello 
+world!")
