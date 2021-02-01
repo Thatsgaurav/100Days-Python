65bbd84334d848c3e523017445b4978bdce93848
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-02-01 18:46:32 +0530

    commit

--- day1.py
+++ day1.py
@@ -13,6 +13,6 @@
 # New lines can be created with a backslash and n.
 
 print("Day 1" + "-" + "String Manipulation")
-print("String Concatenation is done with the "+" sign.")
+print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world"')
 print(("New lines can be created with a backslash and n."))
