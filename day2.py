13c6ed957483483f2231543b32e1459acaa8885d
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-03-11 10:59:46 +0530

    commit

---
+++ day2.py
@@ -0,0 +1,16 @@
+two_digit_number = input("Type a two digit number: ")
+
+####################################
+#Write your code below this line 👇
+
+#Check the data type of two_digit_number
+print(type(two_digit_number))
+
+#Get the first and second digits using subscripting then convert string to int.
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+
+#Add the two digits togeter
+two_digit_number = first_digit + second_digit
+
+print(two_digit_number)
