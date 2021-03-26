07fdac9962758b73c5e24c474b2bbdca03cd5d94
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-03-25 22:37:13 -0500

    commit

--- day3.py
+++ day3.py
@@ -50,6 +50,18 @@
 # Make sure you include the words in bold from the interpretations.
 
 
-heught = float(input("Enter your height in m: "))
+height = float(input("Enter your height in m: "))
 weight = float(input("Enter your wwight in km: "))
 
+
+bmi = round(weight / height ** 2)
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")
