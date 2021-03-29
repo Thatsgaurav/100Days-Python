c4392c91fd4e46667a347a514d0c114503b48155
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-03-29 16:58:24 -0500

    commit

--- day4.py
+++ day4.py
@@ -24,10 +24,23 @@
 # Tails
 # ```
 
-import random
+# import random
 
-random_side = random.randint(0, 1)
-if random_side == 1:
-  print("Heads")
-else:
-  print("Tails")
+# random_side = random.randint(0, 1)
+# if random_side == 1:
+#   print("Heads")
+# else:
+#   print("Tails")
+
+# -------------------------------------- >>>
+
+# Instructions
+# You are going to write a program which will select a random name from a list of names. 
+# The person selected will have to pay for everybody's food bill.
+
+# Important: You are not allowed to use the choice() function.
+
+# Line 8 splits the string names_string into individual names and puts them inside a List 
+# called names. For this to work, you must enter all the names as name followed by comma 
+# then space. e.g. name, name, name
+
