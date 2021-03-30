5638317b7e787024f22cae2276fe900a901a3c04
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2021-03-29 22:21:32 -0500

    commit

--- day4.py
+++ day4.py
@@ -44,3 +44,17 @@
 # called names. For this to work, you must enter all the names as name followed by comma 
 # then space. e.g. name, name, name
 
+# Split string method
+names_string = input("Give me everybody's names, seperated by a comma. ")
+names = names_string.split(", ")
+
+#Write your code below this line 👇
+
+#Get the total number of items in list.
+num_items = len(names)
+#Generate random numbers between 0 and the last index. 
+random_choice = random.randint(0, num_items - 1)
+#Pick out random person from list of names using the random number.
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + " is going to buy the meal today!")
